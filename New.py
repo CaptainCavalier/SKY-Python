dad18578a5b0d3971765452774c77af48b648072
CaptainCavalier <anthonyingegnere@googlemail.com>
2022-10-24 14:30:15 +0100

    updated Repo files

---
+++ New.py
@@ -0,0 +1,21 @@
+
+print("this is my first sub routine test")
+
+def newSub():
+    print ("Hi")
+    print ("we all live in a")
+    print("yellow submarine")
+
+newSub()
+
+#random number generator
+import random
+print (random.randint(0,80))
+
+firstName = "Anthony"
+surname = "Ingegnere"
+
+print(firstName + surname)
+print(firstName, surname)
+print("Anthony")
+print(f"firstName: {firstName} second: {surname}")
---
+++ exercise8.py
@@ -0,0 +1,4 @@
+firstName = "Anthony"
+surname = "Ingegnere"
+
+print (firstName, surname)
