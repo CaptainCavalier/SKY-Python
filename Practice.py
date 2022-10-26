7df878ebfca188020ed82820e6834fc17a1ad7f6
CaptainCavalier <anthonyingegnere@googlemail.com>
2022-10-26 12:32:27 +0100

    Password checker file added

--- Practice.py
+++ Practice.py
@@ -232,21 +232,163 @@ list = [0,1,2,3]
 #     loops += 1
 
 
-
-valid = "no"
-while valid == "no":
-     userin = input("Enter a Number")
-     if userin.isnumeric():
-         print("Thank you for a number")
-         valid = "yes"
-     else:
-         print("that was not a number")
-         valid = "no"
-
-intconv = int(userin)
-userchoice = userin * 2
-print("Carry On")
+#
+# valid = "no"
+# while valid == "no":
+#      userin = input("Enter a Number")
+#      if userin.isnumeric():
+#          print("Thank you for a number")
+#          valid = "yes"
+#      else:
+#          print("that was not a number")
+#          valid = "no"
+#
+# intconv = int(userin)
+# userchoice = userin * 2
+# print("Carry On")
 
 #  *unconditional For
+
+
+# loops = 0
+# while loops < 5:
+#     print("This is a loop", loops)
+#     loops += 1
+#
+# #  ******** UNCONDITIONAL  *******8
+#
+# for i in range(5):
+#     print("this is a loop", i)
+#
+# for i in range(1, 11, 2):
+#     print(f"this is a loop {i}")
+
+mylist = [15,22,83,14,53,6,97]
+
+# for i in range(0, len(mylist)):
+#     print(mylist[i])
+#
+# for nums in mylist:
+#     print(nums)
+#
+# for i in mylist:
+#     if i.isalpha():  #This section didnt work...why
+#         print(i)
+
+# for i in range(120):
+#     print(f"{i} squared is {i*i}")
+#
+# for i in range(120):
+#     if not i % 2 == 0:
+#         print(f"{i} squared is {i*i}")
+#
+# while True:
+#     input("Press Enter to Continue")  #just loops forever
+
+
+# # prime number presenter using user input
+# usernum = int(input("Please Enter a Number"))
+#
+# lower = 1
+# upper = usernum
+# print("Prime numbers between", lower, "and", upper, "are:")
+# for num in range(lower, upper + 1):
+#    # all prime numbers are greater than 1
+#    if num > 1:
+#        for i in range(2, num):
+#            if (num % i) == 0:
+#                break
+#        else:
+#            print(num)
+
+# ******* ENUMERATION *******
+
+
+# list = ["bob", "jim", "tom", "dave"]
+
+# enum = enumerate(list)
+# for i in enum:
+#     print(i)
+
+# for idx, i in enumerate(list):
+#     print(list[idx], i)
+
+# enum = enumerate(list)
+# for i in enum:
+#     print(i[0] + 1, i[1])
+#     print(i)
+
+
+
+# ******* STRINGS *******
+
+# euro = "\u20cc"
+# fourbyte = "\N{euro sign}"
+# print(euro)
+# print(fourbyte)
+#
+
+# chars_as_bytes = b"stores as bytes"
+# print(chars_as_bytes)
+
+
+# print("Hello", end=" " )
+# print("\n")
+# print("World")
+#
+# print("Hello", end="\t" )
+# print("World")
+
+
+# mylist = [2,3,4,5,6,7]
+# for i in mylist:
+#     print(i, end=" ")
+#
+#
+# print("The answer is", 42, "I think", sep=" >> ")
+#
+# print("The answer is", 42, sep=" >> ", end=" ")
+# print("I think")
+
+
+# print(mylist, "and this", "and some more")
+# print(f"{mylist} and this, and some more")
+
+#
+# print( '\r\n \1\2\3 ' )
+# print( r'\r\n \1\2\3')
+
+
+# for i in range(1,11):
+#     for j in range(1, 11):
+#         print(i * j, end=" ")
+
+# print("""
+# sgfdggs grdhdh
+# gerherh          ggrgdrh     greg
+# gerdgrdg5hjy     gredh
+# """)
+#
+
+
+# mystr = ("this will print 'single quotes'")
+# mystr = mystr.replace("this", "that")
+# print(mystr)
+#
+
+# # strip takes "/n"
+#
+# loc = mystr.find("pri")
+# print(loc)
+#
+# print("repeat   " * 4)
+# print(("repeat" + " ") * 4)  #does the same as line above
+
+
+
+
+
+
+
 
 
