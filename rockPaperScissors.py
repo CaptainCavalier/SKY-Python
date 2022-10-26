bcf6165c1dcbe9848ba3fade21b5969b88f847b1
CaptainCavalier <anthonyingegnere@googlemail.com>
2022-10-26 17:06:41 +0100

    New Challenge added

---
+++ rockPaperScissors.py
@@ -0,0 +1,19 @@
+import random
+
+
+RPS = ["Rock", "Paper", "Scissors"]
+
+
+print("Wanna Play A Game?")
+input("Press Enter If You DARE")
+print("It's only Rock, Paper, Scissors, it'll be ok")
+userin = input("Choose Rock, Paper or Scissors: ")
+print("You chose", userin)
+
+# if userin == :
+#     print("The Computer Chose", (random.choice(RPS)))
+
+
+
+
+
